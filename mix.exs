79af5b5bb216b6b2cbecd5dc9fc12020c79762ac
Larry Lv <larrylv1990@gmail.com>
2015-12-12 22:24:54 +0800

    Update mix depencies for docs

--- mix.exs
+++ mix.exs
@@ -3,22 +3,26 @@ defmodule Cowgirl.Mixfile do
 
   def project do
     [app: :cowgirl,
-     version: "0.0.1",
-     description: "Small, fast, modular HTTP server written in Elixir.",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package,
-     deps: deps]
+      version: "0.0.1",
+      description: "Small, fast, modular HTTP server written in Elixir.",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      package: package,
+      deps: deps]
   end
 
   def application do
     [applications: [:logger],
-     mod: {Cowgirl, []}]
+      mod: {Cowgirl, []}]
   end
 
   defp deps do
-    []
+    [
+      {:inch_ex, ">= 0.0.0", only: :docs},
+      {:earmark, "~> 0.1", only: :docs},
+      {:ex_doc, "~> 0.11", only: :docs}
+    ]
   end
 
   defp package do
@@ -26,7 +30,7 @@ defmodule Cowgirl.Mixfile do
       maintainers: ["Larry Lv"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/larrylv/cowgirl",
-               "Docs" => "https://github.com/larrylv/cowgirl"}
+        "Docs" => "https://github.com/larrylv/cowgirl"}
     ]
   end
 end
