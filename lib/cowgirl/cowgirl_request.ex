2d0ecdac6f8163f47145041bebab6c4298915e13
Larry Lv <larrylv1990@gmail.com>
2016-01-14 22:03:29 +0800

    Add module `Cowgirl.Request`

---
+++ lib/cowgirl/cowgirl_request.ex
@@ -0,0 +1,13 @@
+defmodule Cowgirl.Request do
+  defstruct socket: nil,
+            host: "",
+            port: 0,
+            http_version: "",
+            method: "",
+            headers: %{},
+            path: "",
+            query: "",
+            body: "",
+            peer: {}
+
+end
